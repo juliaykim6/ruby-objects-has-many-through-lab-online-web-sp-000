a6923e9b947b087369d3b59b97dc3cf70d765676
Julia Kim <juliaykim6@gmail.com>
2021-02-15 03:24:20 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,7 +18,7 @@ class Doctor
   end
 
   def appointments
-    Appointment.all.select { |appointment| doctor.appointment == self}
+    Appointment.all.select { |appointment| appointment.doctor == self}
   end
 
   def patients
